8639dd18b7fc07d92b4fd61c0b0dfb3a372ff5b1
Big-Wubble <maynorh8936@student.faytechcc.edu>
2024-06-15 20:51:27 -0400

    Add files via upload
    First submission.

---
+++ P1LAB1_MaynorHunter.py
@@ -0,0 +1,9 @@
+#Hunter M.
+#6/15/24
+#P1LAB1
+#Learning how to create a Python program using IDLE.
+
+print('Hello User')
+print('What is your name:', end=' ')
+Name = input()
+print('Hello', Name,', Welcome to my CTI-110 program!')
